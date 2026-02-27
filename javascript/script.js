226b94fc7c741a58db67cb4a03ccb8c413dcc1f3
vitor <vitormwxm@gmail.com>
2026-02-26 22:38:22 -0300

    create modal

--- javascript/script.js
+++ javascript/script.js
@@ -2,18 +2,28 @@
 const buttonAddTask = document.querySelector("#add_button");
 const inputTask = document.querySelector("#add-task");
 const listContainer = document.querySelector("#task_list");
-
+const modal = document.querySelector("#modal-container");
+const closeModalButton = document.querySelector("#close-modal-cancel");
 // --- EVENTOS ---
 window.onload = searchAllTasks;
 buttonAddTask.addEventListener("click", addTask);
 
 listContainer.addEventListener("click", function (event) {
-    const target = event.target.closest(".square");
+    const target = event.target.closest(".square"); 
+    const listItem = event.target.closest("li"); //
+    
+    console.log(listItem);
     if (target) {
-        const listItem = target.parentElement;
         const taskId = listItem.getAttribute("data-task-id");
         deleteTask(listItem, taskId);
+    } else if (listItem) {
+        const taskId = listItem.getAttribute("data-task-id");
+        openModalTaskDetails(taskId);
     }
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.classList.remove("active");
 });
 
 // --- INTERFACE (UI) ---
@@ -22,15 +32,41 @@ function createTaskView(taskTitle, taskId) {
     const listItem = document.createElement("li");
     listItem.setAttribute("data-task-id", taskId);
     listItem.textContent = taskTitle;
-    listItem.classList.add("task-animate"); // Aqui você pode trocar para animateIn se quiser
+    listItem.classList.add("task-animate"); 
 
     const deleteButton = document.createElement("span");
     deleteButton.className = "square";
     deleteButton.innerHTML = '<img src="imgs/icons8-lixeira 1.svg" alt="lixeira" width="30px">';
     
     listItem.appendChild(deleteButton);
-    return listItem; // Retorna o elemento pronto para ser usado
+    return listItem; 
 }
+
+async function openModalTaskDetails(taskId) {
+    console.log(`Abrir modal para tarefa ID: ${taskId}`);
+
+    try {
+        const response = await fetch(`http://localhost:8080/todolist/${taskId}`);
+        if (response.ok) {
+            const task = await response.json();
+
+            document.querySelector("#modal-title").textContent = task.title;
+            document.querySelector("#modal-status").textContent = task.status;
+            document.querySelector("#modal-description").value = task.description || "Sem descrição.";
+            
+            const date = new Date(task.createdAt).toLocaleString('pt-BR');
+            document.querySelector("#modal-date").textContent = date;
+
+            modal.classList.add("active");
+        } else {
+            console.error("Erro ao buscar detalhes da tarefa.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição do modal:", error);
+    }
+}
+
+
 
 // --- SERVIÇOS (API) ---
 
@@ -100,7 +136,7 @@ async function deleteTask(listItem, taskId) {
         });
 
         if (response.ok) {
-            listItem.remove(); // Melhor que searchAllTasks() para manter a animação fluida
+            listItem.remove();
             console.log(`Tarefa ${taskId} deletada.`);
         } else {
             listItem.classList.remove("task-animateOut");
